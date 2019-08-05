315677b96a0d839a2ccaed0e3a1b7a2086a5a492
ALFRED, SKARIA <alfred-skaria@nissanmotor.com>
2019-08-05 17:22:12 +0530

    Full program is added and tested.

--- CPP/Tree/Vertical_Order_Traversal.cpp
+++ CPP/Tree/Vertical_Order_Traversal.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
-
+#include<queue>
+#include<map>
 using namespace std;
 
 struct Node{
@@ -16,8 +17,33 @@ Node *newNode(int data){
 }
 
 void printVerticalOrder(Node *root){
-    
+    if(root == NULL) return;
 
+    int hd = 0;
+    queue<pair<int, Node *>> que;
+    que.push(make_pair(hd,root));
+    map<int, vector<int>> result;
+        while(!que.empty()){
+            pair<int, Node*> temp = que.front();
+            hd = temp.first;
+            Node *node = temp.second;
+            que.pop();
+            result[hd].push_back(node->data);
+
+            if(node->left != NULL){
+                que.push(make_pair(hd-1,node->left));
+            }
+            if(node->right != NULL){
+                que.push(make_pair(hd+1,node->right));
+            }
+        }
+
+        for(auto it : result){
+            for(int i = 0; i<it.second.size(); i++){
+                cout << it.second[i] << " ";
+            }
+            cout << endl;
+        }
     return ;
 }
 
@@ -29,7 +55,7 @@ int main(){
     root->left->left = newNode(3);
     root->left->right = newNode(8);
     root->right->left = newNode(12);
-    root->right->left = newNode(18);
+    root->right->right = newNode(18);
 
     printVerticalOrder(root);
     return 0;
